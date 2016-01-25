4ca1209c8a7659fec33e0d7e4206a54cef7b6273
scmdesai <scmdesai@gmail.com>
2016-01-24 19:41:31 -0600

    test FB Login

--- app.js
+++ app.js
@@ -64872,7 +64872,7 @@ Ext.application({
         Ext.util.Format.undef = function(value, defaultValue) {
             return Ext.isDefined(value) ? value : defaultValue;
         };
-        var ref = cordova.InAppBrowser.open('http://awseb-e-t-AWSEBLoa-YAYDRIIPXYO3-332457445.us-west-2.elb.amazonaws.com/FBLogin.html', '_blank', 'location=yes');
+        var ref = cordova.InAppBrowser.open('http://http://services.appsonmobile.com/FBLogin.html', '_blank', 'location=yes');
         /*
 		var view = Ext.create('Contact.view.Main',{}) ;
 		Ext.create('Contact.store.MyJsonPStore', {
