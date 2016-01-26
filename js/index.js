c1c6e4824d28ae859b7ef820c3c7ea3de4aad874
scmdesai <scmdesai@gmail.com>
2016-01-25 21:21:26 -0600

    changed index.js

--- js/index.js
+++ js/index.js
@@ -18,6 +18,7 @@
  */
 var app = {
     // Application Constructor
+	
     initialize: function() {
         this.bindEvents();
     },
@@ -26,51 +27,54 @@ var app = {
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
     },
+
+
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-	    console.log('deviceready');
-        app.receivedEvent('deviceready');
+		console.log("Device is ready");
+		//this.receivedEvent('deviceready');
 		
+		//StatusBar Overlay set to false
+		StatusBar.overlaysWebView(false);
 		
-		
-	/*	var fbLoginSuccess = function (userData) {
-			alert("UserInfo: " + JSON.stringify(userData));
-			facebookConnectPlugin.getAccessToken(function(token) {
-			alert("Token: " + token);
-		}, function(err) {
-			alert("Could not get access token: " + err);
-		});
-		
-		facebookConnectPlugin.login(["public_profile"], fbLoginSuccess, function (error) 
-		{ 
-			alert("" + error);
-		}
-);
-		
-}*/
-console.log('deviceready');
-//this.facebookAppId = '900651756709444';
-//this.onFacebookInit();
+		/* Commenting out Amazon Analytics */
+		/* Amazon Mobile Analytics*/
+		/*AWS.config.region = 'us-east-1';
+		AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+			IdentityPoolId: 'us-east-1:cd2c5cf7-2b6d-49a6-be5f-5d1a92113389' //Amazon Cognito Identity Pool ID
+		}); 
+		var options = {
+			appId : 'b8f01603ccc24b2c9d0e78b76334febe', //Amazon Mobile Analytics App ID
+			platform: 'Android',
+			// logger: console,  // Commented this line to get rid of the error Uncaught TypeError: Converting circular structure to JSON 
+			appTitle : 'Local Buzz'              //Optional e.g. 'Example App'
+    
+		};
 
-
+		*/
+}
+    
+    }, // end of onDeviceReady function
+	
+    
 		
-		
-    },
+  
     // Update DOM on a Received Event
     receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
-
+  
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
-
+  
         console.log('Received Event: ' + id);
-    }
+	}		
 };
-//app.initialize();
+
+app.initialize();
