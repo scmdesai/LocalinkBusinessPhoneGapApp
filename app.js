071faef6aa4b2bfbb911704173b7ec8f368cf84e
scmdesai <scmdesai@gmail.com>
2016-01-27 13:48:41 -0600

    changes to printing values

--- app.js
+++ app.js
@@ -63681,11 +63681,11 @@ Ext.define('Ext.direct.Manager', {
                     var tmp = info[0].split("\":\"");
                     var email = tmp[1].split("\",\"");
                     tmp = info[1].split("\":\"");
-                    var name = tmp[1];
+                    var name = tmp[1].split("\",\"");
                     tmp = info[2].split("\":\"");
-                    var gender = tmp[1];
+                    var gender = tmp[1].split("\",\"");
                     tmp = info[3].split("\":\"");
-                    var userId = tmp[1];
+                    var userId = tmp[1].split("\",\"");
                     console.log(email);
                     console.log(name);
                     console.log(gender);
