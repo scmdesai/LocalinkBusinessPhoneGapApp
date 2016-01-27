deb9f588030b0cffae4ceff2894e1423f972b3b7
scmdesai <scmdesai@gmail.com>
2016-01-27 12:00:08 -0600

    printing values

--- app.js
+++ app.js
@@ -63677,15 +63677,15 @@ Ext.define('Ext.direct.Manager', {
                 if (userInfo) {
                     var userInf = JSON.stringify(userInfo);
                     console.log(userInf);
-                    var info = userInf.split(",");
+                    var info = userInf.split("\",\"");
                     var tmp = info[0].split("\":\"");
                     var email = tmp[1].split("\",\"");
                     tmp = info[1].split("\":\"");
-                    var name = tmp[1].split("", "");
+                    var name = tmp[1];
                     tmp = info[2].split("\":\"");
-                    var gender = tmp[1].split("", "");
+                    var gender = tmp[1];
                     tmp = info[3].split("\":\"");
-                    var userId = tmp[1].split("", "}");
+                    var userId = tmp[1];
                     console.log(email);
                     console.log(name);
                     console.log(gender);
