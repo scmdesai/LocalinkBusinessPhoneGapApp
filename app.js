1b89719eaadc4d4754c846b8eed16d889ee0ea4d
scmdesai <scmdesai@gmail.com>
2016-01-28 16:46:24 -0600

    print info.get(businessName

--- app.js
+++ app.js
@@ -64505,7 +64505,7 @@ Ext.define('Ext.direct.Manager', {
         var referrer = Ext.Viewport.getActiveItem();
         var form = this.getContactform();
         var info = this.getContactinfo();
-        console.log(info.businessName);
+        console.log(info.get('businessName'));
         form.referrer = referrer;
         form.setValues(info.getValues());
         Ext.Viewport.setActiveItem(form);
