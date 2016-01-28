51fc346db6434bae2447213aea59a45eef610790
scmdesai <scmdesai@gmail.com>
2016-01-27 23:54:52 -0600

    check active item

--- app.js
+++ app.js
@@ -63647,7 +63647,7 @@ Ext.define('Ext.direct.Manager', {
                     console.log(email);
                     var store = Ext.getStore('MyJsonPStore').findRecord('emailAddress', 'jalaram@gmail.com', 0, true, false, false);
                     console.log(store.getData());
-                    var user = this.getParent();
+                    var user = Ext.Viewport.getActiveItem();
                     console.log(user);
                 } else //user.setRecord(store);
                 //Ext.Viewport.setActiveItem(user);
