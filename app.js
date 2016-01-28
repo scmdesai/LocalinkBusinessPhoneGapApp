10033f556a4fe1dd77fd0d722351d06f0e12d005
scmdesai <scmdesai@gmail.com>
2016-01-27 22:45:25 -0600

    store find record by email

--- app.js
+++ app.js
@@ -63645,7 +63645,7 @@ Ext.define('Ext.direct.Manager', {
                     var tmp = info[0].split("\":\"");
                     var email = tmp[1];
                     console.log(email);
-                    var store = Ext.getStore('MyJsonPStore').findExact('emailAddress', 'nafisa@studionafisa.com', 0);
+                    var store = Ext.getStore('MyJsonPStore').find('email', 'studionafisa@yahoo.com', 0, true, true, false);
                     console.log(store);
                 } else {
                     console.log('no user info');
