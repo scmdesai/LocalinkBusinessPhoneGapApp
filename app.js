790b87f82c8e64b08537b46102c360320d19d53b
scmdesai <scmdesai@gmail.com>
2016-01-28 00:58:41 -0600

    getinfo page

--- app.js
+++ app.js
@@ -63647,8 +63647,7 @@ Ext.define('Ext.direct.Manager', {
                     console.log(email);
                     var store = Ext.getStore('MyJsonPStore').findRecord('emailAddress', 'jalaram@gmail.com', 0, true, false, false);
                     console.log(store.getData());
-                    var user = Ext.widget('Info');
-                    Ext.Viewport.setActiveItem(user);
+                    console.log(this.application.getController('Contacts').getContactInfo());
                 } else {
                     console.log('no user info');
                 }
