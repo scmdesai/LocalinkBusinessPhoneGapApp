0bbe8b3cdd90f614c22bc7a31ac5745b61802acc
scmdesai <scmdesai@gmail.com>
2016-01-29 11:26:23 -0600

    cameraPic src

--- app.js
+++ app.js
@@ -64270,7 +64270,7 @@ Ext.define('Ext.direct.Manager', {
                             function uploadPhoto(data) {
                                 // this is where you would send the image file to server
                                 //output image to screen
-                                cameraPic.src = data;
+                                cameraPic.src = "data:image/jpeg;base64," + data;
                                 navigator.notification.alert('Your Photo has been uploaded', // message
                                 okay, // callback
                                 'Photo Uploaded', // title
