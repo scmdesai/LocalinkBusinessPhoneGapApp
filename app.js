c8d5a5474184b01123dc360e3e28017fa656b371
scmdesai <scmdesai@gmail.com>
2016-01-28 23:52:49 -0600

    logging changed record businessname

--- app.js
+++ app.js
@@ -64172,7 +64172,7 @@ Ext.define('Ext.direct.Manager', {
             //console.log('form.referrer is : ' + form.referrer.setRecord);
             //if (record) {
             record.setData(values);
-            console.log('values: ' + values);
+            console.log('values: ' + values.businessName);
             //info.setRecord(record);
             //console.log('Record: ' + record.getData());
             record.commit();
