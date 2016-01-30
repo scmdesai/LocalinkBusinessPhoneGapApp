e274d985665adeddb1df3aeee02276dfedeb4777
scmdesai <scmdesai@gmail.com>
2016-01-30 13:49:34 -0600

    console log on save button tap

--- app.js
+++ app.js
@@ -64258,7 +64258,6 @@ Ext.define('Ext.direct.Manager', {
         //var form = Ext.getCmp('formpanel').getForm();
         var errors = form.getValidationErrors();
         console.log('On Save Button Tap');
-        console.log('Record is :' + record.getData());
         if (errors.length) {
             Ext.Msg.alert('Error', errors.join('<br/>'));
         } else {
