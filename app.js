f77f5f314f8051bd65b90a12eed7c9023069698d
scmdesai <scmdesai@gmail.com>
2016-01-29 21:24:35 -0600

    get form values

--- app.js
+++ app.js
@@ -64256,17 +64256,19 @@ Ext.define('Ext.direct.Manager', {
         if (errors.length) {
             Ext.Msg.alert('Error', errors.join('<br/>'));
         } else {
-            //  var values = form.getValues();
-            var valueBusinessName = form.getAt(3).getValue();
-            var valuePhoneNumber = form.getAt(4).getValue();
-            var valueAddress = form.getAt(5).getValue();
+            var values = form.getValues();
             var record = form.getRecord();
-            record.set('businessName', valueBusinessName);
-            record.set('phoneNumber', valuePhoneNumber);
-            record.set('address', valueAddress);
-            record.commit();
-            if (form.referrer.setRecord) {
-                form.referrer.setRecord(record);
+            console.log('Record is : ' + record);
+            if (record) {
+                console.log('Record is not null');
+                console.log('Values are : ' + values.getData());
+                record.setData(values);
+                record.commit();
+                if (form.referrer.setRecord) {
+                    form.referrer.setRecord(record);
+                }
+            } else {
+                Ext.StoreManager.lookup('MyJsonPStore').add(values);
             }
             Ext.Viewport.setActiveItem(form.referrer);
             delete form.referrer;
