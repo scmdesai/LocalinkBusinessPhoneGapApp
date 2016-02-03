9ff5f2070716184701c6defc419f6aca7dfed782
scmdesai <scmdesai@gmail.com>
2016-02-03 13:55:04 -0600

    list get parent Id

--- app.js
+++ app.js
@@ -67469,7 +67469,7 @@ Ext.define('Ext.picker.Picker', {
                         xtype: 'button',
                         handler: function(button, e) {
                             var list = Ext.getCmp('ListOfDeals');
-                            console.log(list.getStoreId());
+                            console.log(list.getParent());
                         },
                         flex: 1,
                         id: 'DeleteDeal',
