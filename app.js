b745019a414b4f5f0c3a95d643f62e74f4e865b2
scmdesai <scmdesai@gmail.com>
2016-02-05 16:44:32 -0600

    response txt

--- app.js
+++ app.js
@@ -67177,7 +67177,7 @@ Ext.define('Ext.picker.Picker', {
                                     }
                                 },
                                 success: function(success, msg) {
-                                    alert("The server says: " + msg.responseText + success);
+                                    alert("The server says: " + msg.responseText);
                                 }
                             }));
                         },
