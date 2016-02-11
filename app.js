c6ee7bb7adfd0a733f69f64cc0a2513c2348638f
scmdesai <scmdesai@gmail.com>
2016-02-10 22:48:17 -0600

    server response

--- app.js
+++ app.js
@@ -67220,9 +67220,17 @@ Ext.define('Ext.picker.Picker', {
                         success: function() {
                             Ext.Msg.alert('Success');
                         },
-                        failure: function(response) {
-                            Ext.Msg.alert('Failed');
-                            console.log(response);
+                        failure: function(form, action) {
+                            switch (action.failureType) {
+                                case Ext.form.action.Action.CLIENT_INVALID:
+                                    Ext.Msg.alert('Failure', 'Form fields may not be submitted with invalid values');
+                                    break;
+                                case Ext.form.action.Action.CONNECT_FAILURE:
+                                    Ext.Msg.alert('Failure', 'Ajax communication failed');
+                                    break;
+                                case Ext.form.action.Action.SERVER_INVALID:
+                                    Ext.Msg.alert('Failure', action.result.msg);
+                            }
                         }
                     });
                     var storeUserDetails = Ext.getStore('UserDetails');
