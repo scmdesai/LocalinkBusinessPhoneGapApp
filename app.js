c1bd4ce1be37d73ee3d77b1e2a979dd69cb6bdfa
scmdesai <scmdesai@gmail.com>
2016-02-11 17:32:22 -0600

    standrdsubmit to true to check server response

--- app.js
+++ app.js
@@ -66814,6 +66814,7 @@ Ext.define('Ext.picker.Picker', {
         itemId: 'formpanel',
         autoDestroy: false,
         modal: true,
+        standardSubmit: true,
         items: [
             {
                 xtype: 'toolbar',
