809b14159bb755ce0361c72ed3c0e089b7e1d2bf
scmdesai <scmdesai@gmail.com>
2016-02-10 23:54:14 -0600

    console log action result

--- app.js
+++ app.js
@@ -67222,7 +67222,7 @@ Ext.define('Ext.picker.Picker', {
                         },
                         failure: function(form, action) {
                             Ext.Msg.alert('Failure', action.failureType);
-                            console.log(action.result.msg);
+                            console.log(action.result);
                         }
                     });
                     var storeUserDetails = Ext.getStore('UserDetails');
