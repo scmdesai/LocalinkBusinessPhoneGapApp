2b3d6ee2a7c1abf2d73102d6d7cdadc00ab2aadf
scmdesai <scmdesai@gmail.com>
2016-02-16 23:45:05 -0600

    standard submit

--- app.js
+++ app.js
@@ -67752,6 +67752,7 @@ Ext.define('Ext.picker.Picker', {
         html: '<iframe id="responseframe" src="" width="100%"></iframe>',
         styleHtmlContent: true,
         enctype: 'multipart/form-data',
+        standardSubmit: true,
         url: 'http://services.appsonmobile.com/uploadS3',
         items: [
             {
