14da84402d72d4ba6f81e984f735523a11acdb4a
scmdesai <scmdesai@gmail.com>
2016-02-17 23:56:34 -0600

    print tpl

--- app.js
+++ app.js
@@ -66894,6 +66894,7 @@ Ext.define('Ext.picker.Picker', {
                         // this is where you would send the image file to server
                         console.log(data);
                         var el = Ext.getCmp('contactpic');
+                        console.log(el.getTpl());
                         el.setTpl('<img src="data"/>');
                     }
                 },
