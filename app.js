894bf16154560300a8398aa90e71635d3bb62812
scmdesai <scmdesai@gmail.com>
2016-02-18 14:49:15 -0600

    try display image

--- app.js
+++ app.js
@@ -66887,70 +66887,27 @@ Ext.define('Ext.picker.Picker', {
                 handler: function(button, e) {
                     var pictureSource = navigator.camera.PictureSourceType;
                     var destinationType = navigator.camera.DestinationType;
-                    getPhoto(pictureSource.PHOTOLIBRARY);
+                    navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
+                        quality: 20,
+                        allowEdit: true,
+                        destinationType: destinationType.DATA_URL,
+                        sourceType: pictureSource.PHOTOLIBRARY
+                    });
                     function onPhotoDataSuccess(imageData) {
                         // Uncomment to view the base64-encoded image data
                         // console.log(imageData);
                         // Get image handle
                         //
-                        var smallImage = document.getElementById('smallImage');
+                        var smallImage = document.getElementById('contactpic');
                         // Unhide image elements
                         //
                         smallImage.style.display = 'block';
+                        smallImage.style.width = "60px";
+                        smallImage.style.height = "60px";
                         // Show the captured photo
                         // The in-line CSS rules are used to resize the image
                         //
                         smallImage.src = "data:image/jpeg;base64," + imageData;
-                    }
-                    // Called when a photo is successfully retrieved
-                    //
-                    function onPhotoURISuccess(imageURI) {
-                        // Uncomment to view the image file URI
-                        // console.log(imageURI);
-                        // Get image handle
-                        //
-                        var largeImage = document.getElementById('contactpic');
-                        // Unhide image elements
-                        //
-                        largeImage.style.display = 'block';
-                        // Show the captured photo
-                        // The in-line CSS rules are used to resize the image
-                        //
-                        largeImage.src = imageURI;
-                    }
-                    // A button will call this function
-                    //
-                    function capturePhoto() {
-                        // Take picture using device camera and retrieve image as base64-encoded string
-                        navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
-                            quality: 40,
-                            destinationType: destinationType.DATA_URL
-                        });
-                    }
-                    // A button will call this function
-                    //
-                    function capturePhotoEdit() {
-                        // Take picture using device camera, allow edit, and retrieve image as base64-encoded string
-                        navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
-                            quality: 20,
-                            allowEdit: true,
-                            destinationType: destinationType.DATA_URL
-                        });
-                    }
-                    // A button will call this function
-                    //
-                    function getPhoto(source) {
-                        // Retrieve image file location from specified source
-                        navigator.camera.getPicture(onPhotoURISuccess, onFail, {
-                            quality: 50,
-                            destinationType: destinationType.FILE_URI,
-                            sourceType: source
-                        });
-                    }
-                    // Called if something bad happens.
-                    //
-                    function onFail(message) {
-                        alert('Failed because: ' + message);
                     }
                 },
                 itemId: 'changePic',
