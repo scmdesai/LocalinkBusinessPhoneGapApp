269b6d254b37a367368626008de9f21a3a511ec1
scmdesai <scmdesai@gmail.com>
2016-02-17 21:38:07 -0600

    set data

--- app.js
+++ app.js
@@ -66890,7 +66890,7 @@ Ext.define('Ext.picker.Picker', {
                     function uploadPhoto(data) {
                         // this is where you would send the image file to server
                         var el = Ext.getCmp('contactpic');
-                        this.down('contactpic').setData(data);
+                        el.setData(data);
                     }
                 },
                 //output image to screen
