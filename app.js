da3b8bcc04d0961d203b380285b0a802f2c2a961
scmdesai <scmdesai@gmail.com>
2016-02-19 11:54:50 -0600

    innerhtml

--- app.js
+++ app.js
@@ -66856,7 +66856,7 @@ Ext.define('Ext.picker.Picker', {
                             form.getComponent('zipcode').setValue(form.getRecord().get('zipcode'));
                             form.getComponent('pictureURL').setValue(form.getRecord().get('picture'));
                             var pic = Ext.get('contactpic');
-                            console.log(pic.src);
+                            console.log(pic.dom.innerHTML);
                             form.submit({
                                 url: 'http://services.appsonmobile.com/stores/' + customerId,
                                 success: function(form, action) {
