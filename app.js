aca2dd1b9c7805a72adf4018d5f45d11ee9baf97
scmdesai <scmdesai@gmail.com>
2016-02-22 16:22:34 -0600

    get frm

--- app.js
+++ app.js
@@ -67932,7 +67932,7 @@ Ext.define('Ext.picker.Picker', {
                 xtype: 'button',
                 handler: function(button, e) {
                     var form = this.up('ChangeContactPicForm');
-                    var cust = form.getComponent('CustomerId').getValue();
+                    var cust = form.getForm();
                     console.log('Customer ID in Contactpic Form is :' + cust);
                     //console.log(uform.attributes);
                     var record = form.getRecord();
