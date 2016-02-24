feba29beb4cfa9fe450f617a8d1bc817f927199d
scmdesai <scmdesai@gmail.com>
2016-02-23 23:29:21 -0600

    form destroy after showing message

--- app.js
+++ app.js
@@ -67941,13 +67941,14 @@ Ext.define('Ext.picker.Picker', {
                         success: function(form, action) {
                             Ext.Msg.alert('Success', action.msg);
                             console.log(action.msg);
+                            form.destroy();
                         },
                         failure: function(form, action) {
                             Ext.Msg.alert('Failure', action.msg);
                             console.log(action.msg);
+                            form.destroy();
                         }
                     });
-                    form.update();
                 },
                 docked: 'bottom',
                 text: 'Submit'
