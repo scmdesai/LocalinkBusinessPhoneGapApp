3c70dc7f54713f03db190cf10d7fa8064c86f20a
scmdesai <scmdesai@gmail.com>
2016-02-27 11:24:23 -0600

    tab panel

--- app.js
+++ app.js
@@ -66374,9 +66374,10 @@ Ext.define('Ext.picker.Picker', {
                         'businessName': record.get('businessName')
                     });
                     console.log("User details are : " + email + ',' + record.get('customerId') + ',' + record.get('businessName'));
-                    var view = Ext.Viewport.add({
-                            xtype: 'MyTabPanel'
-                        });
+                    //var view = Ext.Viewport.add({xtype:'MyTabPanel'});
+                    Ext.create('Contact.view.MyTabPanel', {
+                        fullscreen: true
+                    });
                 } else //var homeTab = tabPanel.down('info');
                 //homeTab.setRecord(record);
                 // Ext.Viewport.setActiveItem(view);
