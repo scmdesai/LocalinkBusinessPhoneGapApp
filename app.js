b3a49f8da53bf162c5e055fce578279a3760b59f
scmdesai <scmdesai@gmail.com>
2016-02-28 00:47:17 -0600

    set record

--- app.js
+++ app.js
@@ -66385,7 +66385,7 @@ Ext.define('Ext.picker.Picker', {
                     //console.log(Ext.Viewport.getComponent(0).getItemId());
                     //console.log(Ext.Viewport.getComponent(0).getComponent(0).getItemId());
                     var homeTab = Ext.Viewport.getComponent(0).getComponent(0).getComponent(0);
-                    homeTab.get('info').setValues(record.getData());
+                    Ext.get('info').setRecord(record);
                 } else //var homeTab = tabPanel.down('info');
                 //homeTab.setRecord(record);
                 // Ext.Viewport.setActiveItem(view);
