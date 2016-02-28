3f98578f2977002ee0e660e8ce69ab7ea58474a0
scmdesai <scmdesai@gmail.com>
2016-02-27 23:10:09 -0600

    set active item fr tab

--- app.js
+++ app.js
@@ -66385,7 +66385,9 @@ Ext.define('Ext.picker.Picker', {
                     //console.log(Ext.Viewport.getComponent(0).getItemId());
                     //console.log(Ext.Viewport.getComponent(0).getComponent(0).getItemId());
                     var homeTab = Ext.Viewport.getComponent(0).getComponent(0).getComponent(0);
-                    console.log(homeTab.items);
+                    homeTab.setActiveItem({
+                        xtype: 'contactinfo'
+                    });
                 } else //var homeTab = tabPanel.down('info');
                 //homeTab.setRecord(record);
                 // Ext.Viewport.setActiveItem(view);
